b43869a8bea613a1769d956d9ea2cf911f361473
VictorCancino <juanvictorcancino@gmail.com>
2024-08-15 17:39:33 -0600

    se terminó el proyecto 'hola python'

--- hola_python.py
+++ hola_python.py
@@ -1,3 +1,5 @@
 """
 Cuando empiezas en programación el programa esencial que solemos crear para iniciarnos es el mitico 'Hola Mundo',que consisite en imprimir en pantalla dicho mensaje. El reto será crear tu primer programa en Python pero en vez de imprimir el mensaje 'Hola Mundo' imprimiremos el mensaje 'Hola Python' para marcar nuestro inicio en el mundo de la programación muy al estilo Python.
 """
+
+print("Hola Python") #print() es una función precargada por default en Python la cuál nos permite mostrar en consola el valor que se le pasa como argumento dentro de los parentesís. Puede una cadena de texto, una variable, un número, dicionario , tulas, etc.
