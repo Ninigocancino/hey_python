c799838da33d548cf35b89d9d8ff40e4bf30ca51
VictorCancino <juanvictorcancino@gmail.com>
2024-08-15 17:32:17 -0600

    Se agregarón las instrucciones al fichero 'hola_python.py'

---
+++ hola_python.py
@@ -0,0 +1,3 @@
+"""
+Cuando empiezas en programación el programa esencial que solemos crear para iniciarnos es el mitico 'Hola Mundo',que consisite en imprimir en pantalla dicho mensaje. El reto será crear tu primer programa en Python pero en vez de imprimir el mensaje 'Hola Mundo' imprimiremos el mensaje 'Hola Python' para marcar nuestro inicio en el mundo de la programación muy al estilo Python.
+"""
