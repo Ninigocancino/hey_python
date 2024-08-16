ce9918e94a0d25001dda00496c73813ef06c88f3
VictorCancino <juanvictorcancino@gmail.com>
2024-08-15 18:31:12 -0600

    Se completó el proyecto 'mensaje_loco'

---
+++ mensaje.py
@@ -0,0 +1,21 @@
+
+sujeto_1 = "Pepe"
+sujeto_2 = "El fantasma"
+lugar_1 = "cuarto"
+lugar_2 = "el patio"
+conector_1 = "una"
+conector_2 = 'por la'
+algo_1 = "pelota"
+verbo_1 = "olvides"
+verbo_2 = "sacarla"
+verbo_3 = "dormir"
+verbo_4 = "recuerda"
+verbo_5 = "juega"
+tiempo = "noche"
+conector_3 = "no lo"
+verbo_6 = "molestes"
+
+
+print("")
+print(f"{sujeto_1} en el {lugar_1} hay {conector_1} {algo_1} no {verbo_1} {verbo_2} antes de {verbo_3}. {verbo_4} que {sujeto_2} {verbo_5} {conector_2} {tiempo} en {lugar_2} {conector_3} {verbo_6}")
+print("")
