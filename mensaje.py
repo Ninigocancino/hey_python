b9f4ac4bcfcdd61c92140331a217cfc3a9baacd7
VictorCancino <juanvictorcancino@gmail.com>
2024-08-15 18:36:26 -0600

    Se documentó el código del proyecto 'mensaje loco'

--- mensaje.py
+++ mensaje.py
@@ -1,4 +1,5 @@
 
+#Las variables guardan los valores que se susutituirán en el mensaje más adelante
 sujeto_1 = "Pepe"
 sujeto_2 = "El fantasma"
 lugar_1 = "cuarto"
@@ -16,6 +17,8 @@ conector_3 = "no lo"
 verbo_6 = "molestes"
 
 
-print("")
-print(f"{sujeto_1} en el {lugar_1} hay {conector_1} {algo_1} no {verbo_1} {verbo_2} antes de {verbo_3}. {verbo_4} que {sujeto_2} {verbo_5} {conector_2} {tiempo} en {lugar_2} {conector_3} {verbo_6}")
-print("")
+print("") #Línea vacia que da una separaión de líneas al mensaje cuándo se muestre en consola
+
+print(f"{sujeto_1} en el {lugar_1} hay {conector_1} {algo_1} no {verbo_1} {verbo_2} antes de {verbo_3}. {verbo_4} que {sujeto_2} {verbo_5} {conector_2} {tiempo} en {lugar_2} {conector_3} {verbo_6}") #Esto es un f string. Es una forma de poder incluir en un string que se mostrará en pantalla valores almacendados en una variable.
+
+print("") #Línea vacia que da una separaión de líneas al mensaje cuándo se muestre en consola
